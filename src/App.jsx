2ca7f34417dd1be5986abce957251669c93170cd
yorbyn <yorbyn.mollova@gmail.com>
2026-07-26 02:17:09 -0400

    feat: implemet React-router-dom for navegation in pages and create routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
 import MascotasPage from './pages/MascotasPage'
 
 function App(){
     return(
         <>
             <h1><strong>Página de mascotas</strong></h1>
-            <MascotasPage />
+                <header>
+                    <nav>
+                        <NavLink to={"/MascotasPages"}>MascotasPages</NavLink> 
+                    </nav>
+                </header>
+
+            <Routes>
+                <Route path='/MascotasPages' element={<MascotasPage/>}/>  
+            </Routes>
         </>
     )
+    //hacer hijos de mascotaspages (ver detalles, formulario para subir una mascota)
 }
 
 export default App;
