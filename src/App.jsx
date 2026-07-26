ac1a4ad59cc58ab848244ba2f3d80a7e2e0b6ca2
yorbyn <yorbyn.mollova@gmail.com>
2026-07-26 17:52:01 -0400

    feat: manage application for details and comments for the pets in comentariospages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 import MascotasPage from './pages/MascotasPage'
+import ComentariosDetallesPage from "./pages/ComentariosDetallesPage"
+import { Navigate } from 'react-router-dom';
 
 function App(){
     return(
         <>
-            <h1><strong>Página de mascotas</strong></h1>
                 <header>
                     <nav>
-                        <MascotasPage/> 
+                        <h1><strong>Página de mascotas</strong></h1>
+                        <NavLink to="/formulario" >Inscribe a tu mascota aqui</NavLink>
                     </nav>
                 </header>
 
             <Routes>
+                <Route path='/' element={<MascotasPage/>} />
+                <Route path='/comentarios-detalles/:id' element={<ComentariosDetallesPage/>} />
             </Routes>
         </>
     )
-    //hacer hijos de mascotaspages (ver detalles, formulario para subir una mascota)
 }
 
 export default App;
